666fd0cc8e15b4b468f7af2f66c76f818856399b
r0ma <wbfx@ya.ru>
2017-04-25 17:02:51 +0500

    Добавлена страница настроек плагина и сохранение параметров в базе данных.

--- add_youtube_twitch_video_in_posts.php
+++ add_youtube_twitch_video_in_posts.php
@@ -23,27 +23,141 @@ function debug_aytv($arr) {
 // Действия при активации плагина
 register_activation_hook( __FILE__, 'aytvp_activation' );
 function aytvp_activation() {
+
+    if ( get_option( 'youtube_twitch_parser_settings', '' ) == '' ) {
+        $options = [
+            'youtube_category' => '',
+            'youtube_apikey' => '',
+            'twitch_category' => '',
+            'twitch_apikey' => '',
+        ];
+        wp_unslash( $options );
+        update_option( 'youtube_twitch_parser_settings', $options, true );
+    }
+
 }
+
+
+
+
+
+
+
+
+add_filter('the_content', 'add_text_to_content');
+function add_text_to_content($content){
+	$out = $content . "<p>При копировании статьи, ставьте обратную ссылку, пожалуйста!</p>";
+	return $out;
+}
+
+
 
 
 // Создаем страницу плагина
 add_action( 'admin_menu', 'aytv_create_menu' );
 function aytv_create_menu() {
-    add_options_page( 'Add YouTube and Twitch video in posts', 'Youtube / Twitch', 'manage_options', 'aytvp-main-menu', 'aytvp_settings');
+    add_options_page( 'Настройки YouTube and Twitch video in posts', 'Youtube / Twitch', 'manage_options', 'aytvp-main-menu', 'aytvp_settings');
 }
 
 // страница настроек плагина
 function aytvp_settings() {
-    echo "<div>основная страница</div>";
-}
+    // Сохраняем значение API key
+    if ( !empty($_POST['do_save']) ) {
+        $options = [
+            'youtube_category' => $_POST['id_youtube_category'],
+            'youtube_apikey' => $_POST['youtube_apikey'],
+            'twitch_category' => $_POST['id_twitch_category'],
+            'twitch_apikey' => $_POST['twitch_apikey']
+        ];
+        wp_unslash( $options );
+        update_option( 'youtube_twitch_parser_settings', $options, true );
+    }
+
+
+    debug_aytv( get_option( 'youtube_twitch_parser_settings', '' ) );
+
+    // получаем массив с настройками
+    $options = get_option( 'youtube_twitch_parser_settings', '' );
+
+    ?>
+
+    <style>
+        .div-settings {
+            width: 45%;
+            /*height: 100px;*/
+            background-color: #E6E9EB;
+            border-radius: 5px;
+            padding: 10px;
+        }
+    </style>
+
+    <h3>НАСТРОЙКИ ПЛАГИНА</h3>
+    <div class="div-settings">
+        <form action="" method="POST">
+        <table border="0" cellspacing="0" cellpadding="10">
+            <tr>
+                <td>
+                    <strong>Youtube</strong>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <label for="youtube">ID категории Youtube (цифра)</label><br>
+                    <input id="youtube" type="number" min="1" name="id_youtube_category" value="<?php echo $options['youtube_category']; ?>" autocomplete="off">
+                </td>
+
+                <td>
+                    <label for="youtubeapikey">Youtube API key</label><br>
+                    <input id="youtubeapikey" type="text" name="youtube_apikey" value="<?php echo $options['youtube_apikey']; ?>" autocomplete="off">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <strong>Twitch</strong>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="twitch">ID категории Twitch (цифра)</label><br>
+                    <input id="twitch" type="number" min="1" name="id_twitch_category" value="<?php echo $options['twitch_category']; ?>" autocomplete="off">
+                </td>
+
+                <td>
+                    <label for="twitchapikey">Twitch API key</label><br>
+                    <input id="twitchapikey" type="text" name="twitch_apikey" value="<?php echo $options['twitch_apikey']; ?>" autocomplete="off">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="do_save" value="Сохранить">
+                </td>
+            </tr>
+        </table>
+        </form>
+
+
+
+
+    </div>
+
+<?php
+} // aytvp_settings()
 
 
 
 // Добавляем метабоксы
 add_action('add_meta_boxes', 'aytv_add_custom_boxes');
 function aytv_add_custom_boxes() {
-    add_meta_box( 'aytv_meta_box_youtube', 'Youtube', 'meta_box_youtube_id_chanell', 'post', 'side', 'high', null );
-    add_meta_box( 'aytv_meta_box_twitch', 'Twitch', 'meta_box_twitch_id_chanell', 'post', 'side', 'high', null );
+    // $youtube_category = get_option( 'aytv_youtube_category', '' );
+    // $twitch_category = get_option( 'aytv_twitch_category', '' );
+
+    // if ( in_category( $youtube_category ) ) {
+        add_meta_box( 'aytv_meta_box_youtube', 'Youtube', 'meta_box_youtube_id_chanell', 'post', 'side', 'high', null );
+    // }
+    // if ( in_category( $twitch_category ) ) {
+        add_meta_box( 'aytv_meta_box_twitch', 'Twitch', 'meta_box_twitch_id_chanell', 'post', 'side', 'high', null );
+    // }
 }
 
 

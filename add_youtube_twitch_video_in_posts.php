30676235801787b5702328a9cbc906ddedb6299e
r0ma <wbfx@ya.ru>
2017-04-26 18:11:38 +0500

    Добавление видео на страницу работает.

--- add_youtube_twitch_video_in_posts.php
+++ add_youtube_twitch_video_in_posts.php
@@ -20,9 +20,34 @@ function debug_aytv($arr) {
     echo "</pre>";
 }
 
+$options = get_option( 'youtube_twitch_parser_settings', '' );
+define( 'YOUTUBE_CATEGORY', $options['youtube_category'] );
+define( 'YOUTUBE_APIKEY', $options['youtube_apikey'] );
+define( 'TWITCH_CATEGORY', $options['twitch_category'] );
+define( 'TWITCH_APIKEY', $options['twitch_apikey'] );
+
+// url до папки с плагином
+define( 'YT_PLUGIN_DIR', plugin_dir_url(__FILE__) );
+
+
+// url папки с каналами Youtube
+$upload = wp_upload_dir();
+$upload_dir = $upload['basedir'];
+$upload_dir_youtube = $upload_dir . '/youtube-twitch-parser/youtube_channels';
+define( 'YOUTUBE_CHANNELS_DIR', $upload_dir_youtube );
+
+// url папки с каналами Twitch
+$upload = wp_upload_dir();
+$upload_dir = $upload['basedir'];
+$upload_dir_twitch = $upload_dir . '/youtube-twitch-parser/twitch_channels';
+define( 'TWITCH_CHANNELS_DIR', $upload_dir_twitch );
+
+
+
 // Действия при активации плагина
 register_activation_hook( __FILE__, 'aytvp_activation' );
 function aytvp_activation() {
+
 
     if ( get_option( 'youtube_twitch_parser_settings', '' ) == '' ) {
         $options = [
@@ -31,23 +56,127 @@ function aytvp_activation() {
             'twitch_category' => '',
             'twitch_apikey' => '',
         ];
-        wp_unslash( $options );
+        // wp_unslash( $options );
         update_option( 'youtube_twitch_parser_settings', $options, true );
     }
 
+    //  создаем папки для хранения id каналов
+    // СОЗДАЕМ СТРУКТУРУ ПАПОК ДЛЯ ХРАНЕНИЯ ДАННЫХ
+	$upload = wp_upload_dir();
+	$upload_dir = $upload['basedir'];
+
+    // Создаем общую папку `bik` в wp-content/uploads
+    $upload_dir = $upload_dir . '/youtube-twitch-parser';
+
+	if ( ! wp_mkdir_p( $upload_dir ) ) {
+		echo "Не удалось создать директорию <pre>youtube-twitch-parser</pre>";
+        exit();
+	}
+
+	// Создаем пустой файл index.php чтобы исключить просмотр директории
+	file_put_contents( $upload_dir.'/index.php', '<?php // Silence is golden.' );
+
+    // Создаем директорию для youtube каналов
+    wp_mkdir_p( $upload_dir . '/youtube-channels' );
+
+    // Создаем директорию для twitch каналов
+    wp_mkdir_p( $upload_dir . '/twitch-channels' );
 }
 
 
 
 
 
+// функция получения id канала по id поста к которому относится канал
+function get_id_youtube_channel_from_post_id() {
+    global $post;
+    $id_chanell = get_post_meta( $post->ID, _youtube_id_chanell, true );
+    return $id_chanell;
+}
+
+// функция получения id канала по id поста к которому относится канал
+function get_id_twitch_channel_from_post_id() {
+    global $post;
+    $id_chanell = get_post_meta( $post->ID, _twitch_id_chanell, true );
+    return $id_chanell;
+}
 
 
+// Поиск в директории true/false
+// folder может быть == youtube-channels или twitch-channels
+function search_in_dir ( $folder, $file ) {
+    $upload = wp_upload_dir();
+    $upload_dir = $upload['basedir'] . '/youtube-twitch-parser' ;
+    $dir = $upload_dir . '/' . $folder;
+    $file_list = scandir( $dir );
+    return ( in_array( $file, $file_list ) ) ? true : false;
+}
 
-add_filter('the_content', 'add_text_to_content');
-function add_text_to_content($content){
-	$out = $content . "<p>При копировании статьи, ставьте обратную ссылку, пожалуйста!</p>";
-	return $out;
+
+function youtube_parsing( $channel_id, $cnt = 3) {
+    // специальный адрес, отвечающий за выдачу фида
+    $url = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
+         . '&channelId=' . $channel_id
+         . '&order=viewCount'    // упорядочивать по кол-ву просмотров
+         . '&maxResults=' . $cnt  // за раз получать не более $cnt результатов
+        //  . '&fields=items/id/videoId'  // нам нужны только идентификаторы видео
+         . '&key=' . YOUTUBE_APIKEY;
+
+    $buf = file_get_contents( $url );
+
+    // декодируем JSON данные
+    $json = json_decode($buf, true);
+
+    $arr = array();
+
+    # если данных нет —> на выход
+    if ( empty( $json['items'] ) ) return $arr;
+
+    foreach( $json['items'] as $v ) {
+        // $t = array(
+            // 'video_id' => $v['id']['videoId'], #id
+            // 'title' => $v['snippet']['title'], # название
+            // 'desc'  => $v['snippet']['description'], # описание
+            // 'url'   => $v['snippet']['id']['videoId'], # адрес
+        // );
+        // получаем только id лучших видео
+        $t = $v['id']['videoId'];
+        $arr[] = $t;
+    }
+
+    return $arr;
+}
+
+
+add_filter( 'the_content', 'add_video_to_content' );
+function add_video_to_content( $content ) {
+    // global $post;
+    // echo $post->ID;
+
+    if ( in_category( YOUTUBE_CATEGORY ) ) {
+        $channel_id = get_id_youtube_channel_from_post_id();
+        $is_file_video = search_in_dir( 'youtube-channels',  $channel_id. '.txt');
+
+        $file = YOUTUBE_CHANNELS_DIR . '/' . $channel_id . ".txt";
+
+        $links = youtube_parsing( $channel_id );
+        foreach ($links as $id_video) {
+            //  https://www.youtube.com/watch?v=O8W4P2Gre_g
+            $link .= "<iframe width='640' height='360' src='https://www.youtube.com/embed/$id_video?rel=0&amp;controls=0&amp;showinfo=0' frameborder='0' allowfullscreen></iframe><br>";
+            // $link .= 'https://www.youtube.com/watch?v=' . $id_video . '</br>';
+        }
+
+        $out = $content . $link;
+        // $out .= '<br>';
+        // $out .= YOUTUBE_CHANNELS_DIR;
+
+    	return $out;
+    }
+
+    if ( in_category( TWITCH_CATEGORY ) ) {
+    	$out = $content . "<p>TWITCH_CATEGORY TWITCH_CATEGORY TWITCH_CATEGORY</p>";
+    	return $out;
+    }
 }
 
 
@@ -61,7 +190,49 @@ function aytv_create_menu() {
 
 // страница настроек плагина
 function aytvp_settings() {
-    // Сохраняем значение API key
+
+
+    $x = youtube_parsing( 'UCvBGdmck9fnkFa1jWucN87Q' );
+    debug_aytv( $x );
+
+    $channel_id = 'UCvBGdmck9fnkFa1jWucN87Q';
+    $is_file_video = search_in_dir( 'youtube-channels',  $channel_id. '.txt');
+
+    // если файл есть
+    if ( $is_file_video ) {
+        // проверяем дату файла и сравниваем со значением кэша
+        // $file = YOUTUBE_CHANNELS_DIR . '/' . $channel_id . ".txt";
+
+        $upload = wp_upload_dir();
+        $dir = $upload['basedir'] . '/youtube-twitch-parser/youtube-channels' ;
+        $file = $dir . "/" . $channel_id . ".txt";
+        debug_aytv( file_get_contents( $file ) );
+
+        // если кэш устарел
+        if ( time() - @filemtime( $file ) >= 3600 ) {
+            // обновляем данные в файле, т.е ПАРСИМ Youtube
+            echo "кэш устарел";
+        } else echo "кэш ok";
+
+    } else { // если файла нет
+        // парсим Youtube = получаем список id видео
+        $id_video_array = youtube_parsing( $channel_id );
+        $to_text = implode( ' | ', $id_video_array );
+
+        // создаем файл и записываем данные
+        $upload = wp_upload_dir();
+        $dir = $upload['basedir'] . '/youtube-twitch-parser/youtube-channels' ;
+        $file = $dir . "/" . $channel_id . ".txt";
+        file_put_contents( $file, $to_text);
+        chmod( $file, 0777 );
+
+        // этот массив передаем в контент страницы
+
+    }
+
+
+
+    // Сохраняем настройки в базу
     if ( !empty($_POST['do_save']) ) {
         $options = [
             'youtube_category' => $_POST['id_youtube_category'],
@@ -69,12 +240,12 @@ function aytvp_settings() {
             'twitch_category' => $_POST['id_twitch_category'],
             'twitch_apikey' => $_POST['twitch_apikey']
         ];
-        wp_unslash( $options );
+        // wp_unslash( $options );
         update_option( 'youtube_twitch_parser_settings', $options, true );
     }
 
 
-    debug_aytv( get_option( 'youtube_twitch_parser_settings', '' ) );
+    // debug_aytv( get_option( 'youtube_twitch_parser_settings', '' ) );
 
     // получаем массив с настройками
     $options = get_option( 'youtube_twitch_parser_settings', '' );
@@ -135,10 +306,6 @@ function aytvp_settings() {
             </tr>
         </table>
         </form>
-
-
-
-
     </div>
 
 <?php
